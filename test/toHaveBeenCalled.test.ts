a5193d5483141070f1e2534ec131bff51120bc6f
Matthew <matthewjcrosby1@gmail.com>
2023-04-11 13:58:04 +0700

    writeup for test

--- test/toHaveBeenCalled.test.ts
+++ test/toHaveBeenCalled.test.ts
@@ -43,8 +43,32 @@ function makeNumberChoice(data: number) {
   }
 }
 
+// What I'm going for here is that I should expect a function to be called in
+// response to another function.  Initially, I had both the functions
+// here in the same module - which didn't work.  I tried to mock the
+// callback function inside the same module, but jest requires an object be passed
+// The solution I found was to import the entire module *
+// considering it returns an object, we can then spyOn the method from that 
+// module.exports object.
+// In general though, I think this is a bad test.
+// The function should either be passed the callback (in which case we can 
+// mock a callback)
+// or
+// we should test the return value of the higher order value to check 
+// that it does the correct thing rather than checking that it calls the function
+
+// This is a more sensible approach as we're testing the functions as 
+// individual units and not worrying about how they interact with other functions
+// Ultimatley - the tests should probablyy be split into
+// higher order function:  Does it return the correct value based on a given input?
+// callback function: Does it also return the correct value based on a given input?
+// This way, the two functions are tested independantly.
+
+
+
 it('should call the callback with option1 if number arg is less than 10', () => {
   const mock = jest.spyOn(callBackModule, 'aCallback')
+  console.log(callBackModule)
   makeNumberChoice(3)
-  expect(mock).toHaveBeenCalledWith('option1')
+  expect(callBackModule.aCallback).toHaveBeenCalledWith('option1')
 })
