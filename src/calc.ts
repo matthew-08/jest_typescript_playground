9bae5a75e0e83d1174aff37ba3e9389113a4bca5
Matthew <matthewjcrosby1@gmail.com>
2023-04-11 13:36:25 +0700

    function call test

--- src/calc.ts
+++ src/calc.ts
@@ -5,3 +5,9 @@ export function add(x: number, y: number): number {
 export function mul(x: number, y: number): number {
   return x * y;
 }
+
+export function aCallback(data: 'option1' | 'option2') {
+  return data
+}
+export type Callback = typeof aCallback;
+
--- test/toHaveBeenCalled.test.ts
+++ test/toHaveBeenCalled.test.ts
@@ -1,3 +1,6 @@
+import { aCallback } from '../src/calc'
+import * as callBackModule from '../src/calc'
+
 // .toHaveBeenCalled
 // callback should only be called when given a string argument
 function callOnString(callback: (data: string) => void, data: any) {
@@ -5,6 +8,7 @@ function callOnString(callback: (data: string) => void, data: any) {
     callback(data);
   }
 }
+
 
 it('should call the callback if arg is string only', () => {
   const callback = jest.fn()
@@ -20,4 +24,27 @@ it('should not be called if arg is not string', () => {
 
 it('test', () => {
   const arg = jest.fn(() => 'string')()
+})
+
+
+
+// testing toHaveBeenCalledWith
+// if the user chooses a number less than 10, expect
+// aCallback to have been called with option 1
+// else expect with option 2
+
+
+function makeNumberChoice(data: number) {
+  if(data < 10) {
+    return aCallback('option1')
+  }
+  else {
+    return aCallback('option2')
+  }
+}
+
+it('should call the callback with option1 if number arg is less than 10', () => {
+  const mock = jest.spyOn(callBackModule, 'aCallback')
+  makeNumberChoice(3)
+  expect(mock).toHaveBeenCalledWith('option1')
 })
