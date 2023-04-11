b2d5955f5f232671df727b849c8793ce51d3a1f3
Matthew <matthewjcrosby1@gmail.com>
2023-04-11 13:03:00 +0700

    call on string test

--- test/calc.test.ts
+++ test/calc.test.ts
@@ -1,19 +1,6 @@
-
-describe('check for equality', () => {
-  it.only('', () => {
-    const objFactory = () => {
-      return {
-        prop: '1'
-      }
-    }
-    class testObj {
-      prop: string
-      constructor() {
-        this.prop = "1"
-      }
-    }
-    const object1 = objFactory()
-    const object2 = new testObj()
-    expect(object1).toEqual(object2)
-  })
-})
+// callback should only be called when given a string argument
+function callOnString(callback, flavour) {
+  if (typeof flavour === 'string') {
+    callback(flavour);
+  }
+}
